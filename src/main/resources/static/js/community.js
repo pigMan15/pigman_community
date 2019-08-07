fcf9275d54531a11242a59c5c0d82241e90817a2
pigMan15 <153306747@qq.com>
2019-08-07 15:07:58 +0800

    不刷新实现登录

--- src/main/resources/static/js/community.js
+++ src/main/resources/static/js/community.js
@@ -15,7 +15,15 @@ function post(){
            if(response.code == 200){
                 $("#comment_section").hide();
            }else{
-               alert(response.message)
+               if(response.code == 2003){
+                   var isLogin = confirm("未登录，是否登录？");
+                   if(isLogin){
+                       window.open("https://github.com/login/oauth/authorize?client_id=3605232d8c7cceafdbe2&redirect_uri=http://localhost:8080/callback&scope=user&state=1");
+                       window.localStorage.setItem("closable",true);
+                   }
+               }else{
+                   alert(response.message)
+               }
            }
        },
        dataType:"json"
